66f2811899f7a2c3d42fd7fafb745da84cb6588d
T4nya13 <tanyatannu4321@gmail.com>
2026-07-09 22:06:31 +0530

    chore: enable static export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
-    // This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // This allows production builds to successfully complete even if
-    // your project has TypeScript type assignment errors.
     ignoreBuildErrors: true,
   }
 };
